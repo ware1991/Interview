921bbf0b12375dd238bae41b2ef6b5c33043ffea
Ricky <michell.hong1991@gmail.com>
2018-12-20 00:29:59 +0800

    Setup AsiaYoTest

--- tests/Unit/AsiaYoTest.php
+++ tests/Unit/AsiaYoTest.php
@@ -3,17 +3,11 @@
 namespace Tests\Unit;
 
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class ExampleTest extends TestCase
+class AsiaYoTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBasicTest()
+    public function setUp()
     {
-        $this->assertTrue(true);
+        parent::setUp();
     }
 }
