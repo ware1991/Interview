93a4abda8ea08687aed5544537fe81777391730d
Ricky <michell.hong1991@gmail.com>
2018-12-20 14:30:18 +0800

    Format a numeric with grouped thousands: test format thousand numeric  @AsiaYoTest

--- tests/Unit/AsiaYoTest.php
+++ tests/Unit/AsiaYoTest.php
@@ -10,4 +10,30 @@ class AsiaYoTest extends TestCase
     {
         parent::setUp();
     }
+
+    /**
+     * 請使用你/妳擅長的語言實作一個 numeric format string function，依照每三位數標出逗號。
+     * First case format thousand numeric
+     * e.g.
+     * f(9527) => “9,527”
+     *
+     * @test
+     * @group AsiaYoTest
+     */
+    public function assertThousandNumeric()
+    {
+        $this->assertEquals("9,527", $this->numericFormat("9527"));
+    }
+
+    /**
+     * Format a numeric with grouped thousands
+     *
+     * @param string $numeric The numeric being formatted.
+     *
+     * @return string A formatted version of numeric.
+     */
+    private function numericFormat(string $numeric)
+    {
+        return number_format($numeric);
+    }
 }
